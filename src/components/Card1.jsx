9c4c3d55a79f5e62828ebafe2644e19e292bf158
farisahmdali <farisahmdali@gmai.com>
2023-11-23 07:37:38 +0530

    small change

--- src/components/Card1.jsx
+++ src/components/Card1.jsx
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
 function Card1({ description, head, price,link }) {
   return (
     <div className=" bg-white border border-gray-200 shadow relative">
-      <a href="#">
-        <img className="" src={image1} />
-      </a>
+      <div>
+        <img  src={image1} alt="img"/>
+      </div>
       <div className="p-5">
-        <a href="#">
+        <div>
           <h5 className="mb-2 text-2xl font-[400] tracking-tight text-gray-600 ">
             {head}
           </h5>
-        </a>
+        </div>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {description}
         </p>
